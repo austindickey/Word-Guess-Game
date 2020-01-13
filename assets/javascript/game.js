39ace6be32915ab3508abdffa4a04a6d8060bb32
austindickey <austindickey@me.com>
2020-01-13 17:31:26 -0600

    reworking game logic completely

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,3 @@
-/* It pains me to say, but for once, I have to allow myself to submit something incomplete; 
-due to my extenuating travel circumstances. This should be the 1 and only time this happens. */
-
 var wordList = [
     { word: "bolt", char: 4, guesses: 7, sum: 49 },
     { word: "grime", char: 5, guesses: 9, sum: 52 },
@@ -20,64 +17,88 @@ var wordIndex = 0
 var wins = 0
 var losses = 0
 var adder = 0
+var blanks = []
+
+function clearContent() {
+    document.querySelector("#word").innerHTML = "Correct letters guessed: "
+    blanks = []
+}
+
+function createBlanks() {
+    clearContent()
+    for (var k = 0; k < wordList[wordIndex].char; k++) {
+        blanks.push("_")
+    }
+
+    document.getElementById("word").innerHTML = blanks.join(" ")
+}
 
 function endGame() {
+
+    if (wordList[wordIndex].guesses === 0) {
+        losses++
+        wordIndex++
+        adder = 0
+        createBlanks();
+    }
+
+    if (adder === wordList[wordIndex].sum && wordList[wordIndex].guesses !== 0) {
+        wins++
+        wordIndex++
+        adder = 0
+        createBlanks();
+    }
+
     if (wordIndex >= wordList.length) {
         document.querySelector("#masterBox").innerHTML = "Game Over!"
         return
     }
 }
 
-function clearContent() {
-    document.querySelector("#word").innerHTML = "Correct letters guessed: ";
-}
-
 document.onkeyup = function (event) {
     var keyPress = event.key.toLowerCase()
-    var corrects = document.querySelector("#word");
-
-    if (wordIndex === wordList.length) {
-        return
-    }
+    var corrects = document.querySelector("#word")
 
     document.querySelector("#wins").innerHTML = "Wins: " + wins
     document.querySelector("#losses").innerHTML = "Losses: " + losses
     document.querySelector("#characters").innerHTML = "Number of letters in word: " + wordList[wordIndex].char
 
     if (keyPress === "a" || keyPress === "b" || keyPress === "c" || keyPress === "d" || keyPress === "e" || keyPress === "f" || keyPress === "g" || keyPress === "h" || keyPress === "i" || keyPress === "j" || keyPress === "k" || keyPress === "l" || keyPress === "m" || keyPress === "n" || keyPress === "o" || keyPress === "p" || keyPress === "q" || keyPress === "r" || keyPress === "s" || keyPress === "t" || keyPress === "u" || keyPress === "v" || keyPress === "w" || keyPress === "x" || keyPress === "y" || keyPress === "z") {
-        wordList[wordIndex].guesses--
-        // add in if statement for if keyPress has already been pressed, don't remove a guess from the user again -- not done
-        document.querySelector("#guesses").innerHTML = "Guesses Left: " + wordList[wordIndex].guesses
-        if (wordList[wordIndex].guesses === 0) {
-            losses++
-            wordIndex++
-            adder = 0
-            clearContent()
-            endGame()
-        }
-
-        // There might be something wrong with the below statement in the scenario that all the letters have been guessed but the .guesses is still not at 0
-        if (adder === wordList[wordIndex].sum && wordList[wordIndex].guesses !== 0 || adder === wordList[wordIndex].sum && wordList[wordIndex].guesses === 0 ) {
-            wins++
-            wordIndex++
-            adder = 0
-            clearContent()
-            endGame()
-        }
     }
 
     else {
         alert("Invalid key was pressed")
     }
 
-    for (var i = 0; i < wordList[wordIndex].char; i++) {
-        if (keyPress === wordList[wordIndex].word[i]) {
-            corrects.textContent += keyPress;
-            adder += alphabetScore[keyPress]
-            return
+    function checkChars(char) {
+
+        var goodChar = false
+
+        for (var i = 0; i < wordList[wordIndex].char; i++) {
+            if (wordList[wordIndex].word[i] === char) {
+                goodChar = true
+            }
         }
-        // I can't figure out how to tell it if keypress has already been pressed once in this cycle, don't print or calculate it
+
+        if (goodChar) {
+            for (var j = 0; j < wordList[wordIndex].char; j++) {
+                if (wordList[wordIndex].word[j] === char) {
+                    adder += alphabetScore[keyPress]
+                    blanks[j] = keyPress
+                    corrects.textContent = blanks
+                    return
+                }
+            }
+        }
+
+        else {
+            wordList[wordIndex].guesses--
+            document.querySelector("#guesses").innerHTML = "Guesses Left: " + wordList[wordIndex].guesses
+        }
 
     }
+
+    checkChars(keyPress)
+    endGame()
 
 }
