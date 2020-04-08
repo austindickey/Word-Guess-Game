e633cf2f0ad55b801cf7ded50df0343ab0c6bc27
austindickey <austindickey@me.com>
2020-04-08 15:45:11 -0500

    converting game to jquery and updating some logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 var wordList = [
-    { word: "bolt", char: 4, guesses: 7, sum: 49 },
-    { word: "grime", char: 5, guesses: 9, sum: 52 },
-    { word: "wings", char: 5, guesses: 9, sum: 72 },
-    { word: "guitar", char: 6, guesses: 11, sum: 76 },
-    { word: "drifter", char: 7, guesses: 13, sum: 80 },
-    { word: "bank", char: 4, guesses: 7, sum: 28 },
-    { word: "onion", char: 5, guesses: 9, sum: 67 }
+    { word: "bolt", char: 4, sum: 49 },
+    { word: "grime", char: 5, sum: 52 },
+    { word: "wings", char: 5, sum: 72 },
+    { word: "guitar", char: 6, sum: 76 },
+    { word: "drifter", char: 7, sum: 80 },
+    { word: "bank", char: 4, sum: 28 },
+    { word: "onion", char: 5, sum: 67 }
 ]
 
 // This game will be based of an alphabet score system. If the letters do not add up to the sum, it will be a loss.
@@ -17,8 +17,8 @@ var wordIndex = 0
 var wins = 0
 var losses = 0
 var adder = 0
+var guesses = 10
 var blanks = []
-var first = true
 
 function createBlanks() {
     blanks = []
@@ -26,84 +26,125 @@ function createBlanks() {
         blanks.push("_")
     }
 
-    document.querySelector("#word").innerHTML = blanks.join(" ")
+    // What blanks looks like after the loop:
+    // blanks = ["_","_","_","_"]
+
+    blanks.join(" ")
+
+    // What I'm expecting:
+    // _ _ _ _
+
+    // What I'm getting:
+    // _,_,_,_
+
+    $("#word").text(blanks)
 }
 
 function newWord() {
     createBlanks()
-    document.querySelector("#wrongLetters").innerHTML = "Wrong Letters Guessed: "
-    document.querySelector("#guesses").innerHTML = "Guesses Left: "
+    $("#wrongLetters").text("Wrong Letters Guessed: ")
+    $("#guesses").text("Guesses Left: " + guesses)
+}
+
+function displayScore() {
+    $("#wins").text("Wins: " + wins)
+    $("#losses").text("Losses: " + losses)
 }
 
 function endGame() {
     if (wordIndex >= wordList.length) {
-        document.querySelector("#masterBox").innerHTML = "Game Over!"
-        return
+        $("#masterBox").text("Game Over!")
+        addStartButton()
     }
 
-    if (wordList[wordIndex].guesses === 0) {
+    if (guesses === 0) {
         losses++
         wordIndex++
         adder = 0
+        guesses = 10
+        displayScore()
         newWord()
     }
 
-    if (adder === wordList[wordIndex].sum && wordList[wordIndex].guesses !== 0) {
+    if (adder === wordList[wordIndex].sum && guesses !== 0) {
         wins++
         wordIndex++
         adder = 0
+        guesses = 10
+        displayScore()
         newWord()
     }
 }
 
-document.onkeyup = function (event) {
-    var keyPress = event.key.toLowerCase()
-
-    if (first) {
-        newWord()
-        first = false
-    }
-
-    var corrects = document.querySelector("#word")
-
-    document.querySelector("#wins").innerHTML = "Wins: " + wins
-    document.querySelector("#losses").innerHTML = "Losses: " + losses
-
-    if (keyPress === "a" || keyPress === "b" || keyPress === "c" || keyPress === "d" || keyPress === "e" || keyPress === "f" || keyPress === "g" || keyPress === "h" || keyPress === "i" || keyPress === "j" || keyPress === "k" || keyPress === "l" || keyPress === "m" || keyPress === "n" || keyPress === "o" || keyPress === "p" || keyPress === "q" || keyPress === "r" || keyPress === "s" || keyPress === "t" || keyPress === "u" || keyPress === "v" || keyPress === "w" || keyPress === "x" || keyPress === "y" || keyPress === "z") {
-    }
-
-    else {
-        alert("Invalid key was pressed")
-    }
-
-    function checkChars(char) {
-        var goodChar = false
-
-        for (var i = 0; i < wordList[wordIndex].char; i++) {
-            if (wordList[wordIndex].word[i] === char && blanks.includes(keyPress)) {
-                goodChar = false
-            }
-            else if (wordList[wordIndex].word[i] === char) {
-                goodChar = true
-            }
-        }
-
-        if (goodChar) {
-            for (var j = 0; j < wordList[wordIndex].char; j++) {
-                if (wordList[wordIndex].word[j] === char) {
-                    adder += alphabetScore[keyPress]
-                    blanks[j] = keyPress
-                }
-            }
-            corrects.textContent = blanks
-        }
-
-        else {
-            document.querySelector("#guesses").innerHTML = "Guesses Left: " + wordList[wordIndex].guesses--
-            document.querySelector("#wrongLetters").append(keyPress + " ")
-        }
-    }
-
-    endGame()
-    checkChars(keyPress)
+function addStartButton() {
+    $("#word").html("<button id='startGame' type='button' class='btn btn-secondary'>Start Game</button>")
 }
+
+var duplicateGuesses = []
+var badGuesses = []
+
+function checkChars(keyPress) {
+    var goodChar = false
+    var corrects = $("#word")
+    
+
+    for (var i = 0; i < wordList[wordIndex].char; i++) {
+        if (wordList[wordIndex].word[i] === keyPress && blanks.includes(keyPress)) {
+            goodChar = false
+            // console.log(goodChar)
+        } else if (wordList[wordIndex].word[i] === keyPress) {
+            goodChar = true
+            // console.log(goodChar)
+        }
+    }
+
+    if (goodChar) {
+        for (var j = 0; j < wordList[wordIndex].char; j++) {
+            if (wordList[wordIndex].word[j] === keyPress) {
+                adder += alphabetScore[keyPress]
+                blanks[j] = keyPress
+            }
+        }
+        blanks.join(" ")
+        corrects.text(blanks)
+        // console.log("Correct: " + blanks)
+        endGame()
+    } else {
+        if (jQuery.inArray(keyPress, duplicateGuesses) === -1){
+            $("#guesses").text("Guesses Left: " + guesses--)
+            $("#wrongLetters").append(keyPress + " ")
+        } else {
+            badGuesses.push(keyPress)
+            duplicateGuesses.push(keyPress)
+            console.log("bad: " + badGuesses)
+            console.log("dup from else: " + duplicateGuesses)
+        }
+        endGame()
+    }
+}
+
+function startGame() {
+    // var keyPress = event.key.toLowerCase()
+
+    displayScore()
+
+    newWord()
+
+    $(document).keydown(function (e) {
+        if (e.keyCode >= 65 && e.keyCode <= 90) {
+            var keyPress = e.originalEvent.key
+            // console.log(keyPress)
+            checkChars(keyPress)
+        }
+    })
+}
+
+// Listeners
+$(document).ready(function () {
+    addStartButton()
+
+    $("#startGame").on("click", function () {
+        startGame()
+    })
+})
+
